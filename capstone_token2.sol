b2b9371ae1664e651e84bd41d41f01fc841435cb
sankaralingam <122398798+sankaralingamv@users.noreply.github.com>
2023-11-06 20:51:43 +0530

    Create capstone_token2.sol

---
+++ capstone_token2.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SKT is ERC20 {
+  constructor() ERC20("StacKoin Token", "SKT") {
+    _mint(msg.sender, 1000000 * (10 ** decimals()));
+  }
+
+}
